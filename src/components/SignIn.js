e32aa2916e4daf9de301d644b3b2bf849790de12
nekorytaylor666 <art_gulba@mail.ru>
2019-09-20 12:35:48 +0900

    initial commit

---
+++ src/components/SignIn.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+
+
+import * as actions from '../actions';
+import CustomInput from './CustomInput';
+
+class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+
+  }
+
+  async onSubmit(formData) {
+    const { history } = this.props;
+
+    await this.props.signIn(formData);
+    if (!this.props.errorMessage) {
+      history.push('/dashboard');
+    }
+  }
+
+
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <div className="row">
+        <div className="col">
+          <form onSubmit={handleSubmit(this.onSubmit)}>
+            <fieldset>
+              <Field
+                name="email"
+                type="text"
+                id="email"
+                label="Enter your email"
+                placeholder="example@example.com"
+                component={ CustomInput } />
+            </fieldset>
+            <fieldset>
+              <Field
+                name="password"
+                type="password"
+                id="password"
+                label="Enter your password"
+                placeholder="yoursuperpassword"
+                component={ CustomInput } />
+            </fieldset>
+
+            { this.props.errorMessage ?
+            <div className="alert alert-danger">
+              { this.props.errorMessage }
+            </div> : null }
+
+            <button type="submit" className="btn btn-primary">Sign In</button>
+          </form>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    errorMessage: state.auth.errorMessage
+  }
+}
+
+export default compose(
+  connect(mapStateToProps, actions),
+  reduxForm({ form: 'signin' })
+)(SignIn)
